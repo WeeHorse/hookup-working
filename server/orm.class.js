1a410e8011cc65433ca0f20fa15fdcb9f7840e26
WeeHorse <benjaminberglund@gmail.com>
2017-04-29 21:51:14 +0200

    added Route that parses route data

--- server/orm.class.js
+++ server/orm.class.js
@@ -1,4 +1,5 @@
-var System = require('./system.class');
+var System = require('./system.class'),
+    Route = require('./route.class');
 
 module.exports = class Orm extends System{
 
@@ -7,10 +8,42 @@ module.exports = class Orm extends System{
     this.db = this.modules.mysql.createPool(this.config.db);
   }
 
+  // mapRoute(req){
+  //   var parts = req.url.split('/');
+  //   this.route = {
+  //     method: req.method,
+  //     type: null,
+  //     table: null,
+  //     id: null,
+  //     filter: null,
+  //     sort: null
+  //   };
+  //   this.route.type = parts.shift();
+  //   if(this.route.type.indexOf(['concepts', 'entities', 'views']) < 0){
+  //     return false;
+  //   }
+  //   this.route.table = parts.shift();
+  //   if(!this.route.table){ // or not in db
+  //     return false;
+  //   }
+  //   var filter = parts.shift();
+  //   if(filter && Number.isSafeInteger(filter/1)){
+  //     this.route.id = filter;
+  //   }else if(filter){
+  //     this.route.filter = filter;
+  //   }
+  //   var sort = parts.shift();
+  //   if(sort){
+  //     this.route.sort = sort;
+  //   }
+  //   return true;
+  // }
+
   middleware(req, res, next){
-    console.log('req', req);
-    if(true){
-      res.json({a:1});
+    var route = new Route(req);
+    if(route.valid){
+      console.log(route);
+      res.json(route);
       res.end();
     }else{
       next();
---
+++ server/route.class.js
@@ -0,0 +1,28 @@
+module.exports = class Route{
+
+  constructor(req){
+    this.method = req.method;
+
+    var parts = req.url.replace(/^\//,'').split('/');
+
+    var type = parts.shift();
+    this.type = (type && ['concepts', 'entities', 'views'].indexOf(type) > -1)? type : null;
+
+    var table = parts.shift();
+    this.table = (table && this.inDb(table))? table : null;
+
+    var filter = parts.shift();
+    this.id = (filter && Number.isSafeInteger(filter/1))? filter : null;
+    this.filter = (filter && !this.id)? filter : null;
+
+    var sort = parts.shift();
+    this.sort = sort || null;
+
+    this.valid = this.method && this.type && this.table;
+  }
+
+  inDb(table){
+    return true;
+  }
+
+}
